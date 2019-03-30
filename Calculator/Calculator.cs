7dda7895505f1fe8a4c65d87be9945ad8b4f0032
t-ignatovich <49087833+t-ignatovich@users.noreply.github.com>
2019-03-30 11:27:09 +0300

    Add Sum function

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -6,6 +6,15 @@ public static class Calculator
     public static void Sum()
     {
         Console.WriteLine();
+
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+
+        Console.WriteLine();
     }
 
     // выводит на консоль результат вычитания двух целых чисел
