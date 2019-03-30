98fc524e5a90d1756ce0bf7c7f761658f1f8b815
t-ignatovich <49087833+t-ignatovich@users.noreply.github.com>
2019-03-30 11:31:16 +0300

    Add Division function

--- Calculator/Calculator.cs
+++ Calculator/Calculator.cs
@@ -24,5 +24,14 @@ public static class Calculator
     public static void Division()
     {
         Console.WriteLine();
+
+        Console.Write("Введите первое число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int num2 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+
+        Console.WriteLine();
     }
 }
