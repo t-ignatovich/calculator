9e475ac8fbba1345523c6b2e4f5b66c385957be5
t-ignatovich <49087833+t-ignatovich@users.noreply.github.com>
2019-03-30 11:17:29 +0300

    Init project
    Первичная инициализация проекта / скелет проекта

---
+++ Calculator/AppEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public static class AppEntry
+{
+    // главная точка входа для приложения
+    private static void Main()
+    {
+        string command = "";
+
+        while (true)
+        {
+            Console.Write("> ");
+            command = Console.ReadLine();
+
+            switch (command)
+            {
+                case "sum":
+                    Calculator.Sum();
+                    break;
+                case "sub":
+                    Calculator.Subtraction();
+                    break;
+                case "mult":
+                    Calculator.Multiplication();
+                    break;
+                case "div":
+                    Calculator.Division();
+                    break;
+                case "exit":
+                    return;
+
+                default:
+                    Console.WriteLine(Environment.NewLine +
+                                      "ОШИБКА: Команда не распознана." +
+                                      Environment.NewLine);
+                    break;
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class Calculator
+{
+    // выводит на консоль результат сложения двух целых чисел
+    public static void Sum()
+    {
+        Console.WriteLine();
+    }
+
+    // выводит на консоль результат вычитания двух целых чисел
+    public static void Subtraction()
+    {
+        Console.WriteLine();
+    }
+
+    // выводит на консоль результат произведения двух целых чисел
+    public static void Multiplication()
+    {
+        Console.WriteLine();
+    }
+
+    // выводит на консоль результат деления двух целых чисел
+    public static void Division()
+    {
+        Console.WriteLine();
+    }
+}
